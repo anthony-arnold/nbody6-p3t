e7faab7d7e3322b3aaf94243099cd7b3f0860d5b
Anthony Arnold <anthony.arnold@uqconnect.edu.au>
2022-01-26 20:53:44 +1000

    Increase rate of GC integration.

--- Ncode/gcint.f
+++ Ncode/gcint.f
@@ -9,7 +9,7 @@
      &       MBULGE,RBULGE,MDISK,SCALEA,SCALEB,MHALO,RHALO,VCIRC
       REAL*8  FM(3),FD(3),FS(3),FSD(3)
       REAL*8 DTGC
-      parameter (DTGC=0.0009765625)  ! 1/1024
+      parameter (DTGC=0.00048828125)  ! 1/2048
 *
 *
 *       Predict coordinates and velocities to order FDOT.
