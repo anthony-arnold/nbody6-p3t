7c73d074cefe0f03e787b7b82d0fed8e621f38a3
Anthony Arnold <anthony.arnold@uqconnect.edu.au>
2022-04-08 20:57:38 +1000

    Modify leapfrog algorithm to make way for SEV.

--- GPU2/intgrt.leapfrog.f
+++ GPU2/intgrt.leapfrog.f
@@ -42,29 +42,18 @@
       END DO
 !     $omp end parallel do
 *
-*     Half-kick
-      CALL STOPWATCH(TBEG)
-!     $omp parallel do schedule(runtime)
-      DO J = IFIRST,NTOT
-         DO K=1,3
-            X0DOT(K,J) = X0DOT(K,J) + FR(K,J)*STEPLF2
-         END DO
-         T0R(J) = TIME + SMAX*0.5D0
-      END DO
-!     $omp end parallel do
-      CALL STOPWATCH(TEND)
-      TINTRE = TINTRE + TEND - TBEG
-*
 *     Set new time and save block time
  1    TIME = TIME + SMAX
       TTOT = TIME + TOFF
 *
-*     Drift
+*     Half Drift
+***   T_pos = T_vel + 0.5
+*
       CALL STOPWATCH(TBEG)
 !     $omp parallel do schedule(runtime)
       DO J = IFIRST,NTOT
          DO K=1,3
-            X0(K,J) = X0(K,J) + X0DOT(K,J)*STEPLF
+            X0(K,J) = X0(K,J) + X0DOT(K,J)*STEPLF2
          END DO
       END DO
 !     $omp end parallel do
@@ -147,14 +136,9 @@
       END DO
       END IF
 *
-*       Check next adjust time before beginning a new block.
-      IF (TIME.GE.TADJ) THEN
-          TIME = TADJ
-          IPHASE = 3
-          GO TO 100
-      END IF
-*
 *     Full-kick
+***   T_vel = T_pos + 0.5
+*
       CALL STOPWATCH(TBEG)
 !     $omp parallel do schedule(runtime)
       DO J = IFIRST,NTOT
@@ -166,6 +150,26 @@
 !     $omp end parallel do
       CALL STOPWATCH(TEND)
       TINTRE = TINTRE + TEND - TBEG
+*
+*
+*     Half Drift
+      CALL STOPWATCH(TBEG)
+!     $omp parallel do schedule(runtime)
+      DO J = IFIRST,NTOT
+         DO K=1,3
+            X0(K,J) = X0(K,J) + X0DOT(K,J)*STEPLF2
+         END DO
+      END DO
+*
+*
+*** HERE: T_pos = T_vel
+*
+*       Check next adjust time before beginning a new block.
+      IF (TIME.GE.TADJ) THEN
+          TIME = TADJ
+          IPHASE = 3
+          GO TO 100
+      END IF
 *
 *       Advance counters and check timer & optional COMMON save (NSUB = 0).
       NTIMER = NTIMER + NN
@@ -220,23 +224,8 @@
 *       Close GPU & GPUIRR library and stop.
       CALL GPUNB_CLOSE
       STOP
-*
-*     Half-kick to catch up.
- 100  CALL STOPWATCH(TBEG)
-!     $omp parallel do schedule(runtime)
-      DO J = IFIRST,NTOT
-         DO K=1,3
-            X0DOT(K,J) = X0DOT(K,J) + FR(K,J)*STEPLF2
-         END DO
-         T0R(J) = TIME
-         T0(J) = TIME
-      END DO
-!     $omp end parallel do
-      CALL STOPWATCH(TEND)
-      TINTRE = TINTRE + TEND - TBEG
-
 *       Check optional mass loss time at end of block-step.
-      IF (KZ(19).GT.0) THEN
+ 100  IF (KZ(19).GT.0) THEN
 *       Delay until time commensurate with 100-year step (new polynomials).
           IF (TIME.GT.TMDOT.AND.DMOD(TIME,STEPX).EQ.0.0D0) THEN
               IF (KZ(19).GE.3) THEN
@@ -246,7 +235,6 @@
               END IF
           END IF
       END IF
-
 *     COPY for adjust
 !     $omp parallel do
  105  DO I = IFIRST,NTOT
